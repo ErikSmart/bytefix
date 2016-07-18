472684298187ee67f92511712764b397c0c7a602
“ErikSmart” <erikcat06@gmail.com>
2016-07-17 20:43:27 -0500

    Se me olvido guardar

---
+++ enviar.php
@@ -0,0 +1,12 @@
+<?php
+$nombre = $_POST['nombre'];
+$email = $_POST['correo'];
+$mensaje = $_POST['asunto'];
+$datos_fromulario=" Nombre: $nombre \n Correo: $email \n Asunto: $mensaje \n";
+$recipient = "erikcat06@yahoo.com";
+$subject = "Mensaje del sitio web";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $datos_fromulario, $mailheader) or die("Error!");
+echo "En menos de dos días nos comunicaremos";
+header( "refresh:1;http://www.bytefix.esy.es" );
+?>
